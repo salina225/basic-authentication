0ed27875393c61972c3fecb6bd04d8c1e14f6d7f
salina225 <70594148+salina225@users.noreply.github.com>
2021-04-07 12:25:43 -0500

    copy/paste reformatted

---
+++ src/Nav.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Menu } from 'antd';
+import {
+    HomeOutlined
+    , ProfileOutlined
+    , FileProtectOutlined
+} from '@ant-design/icons';
+
+const Nav = ({ current }) => {
+    return (
+        <div>
+            <Menu
+                selectedKeys={[current]}
+                mode="horizontal"
+            >
+                <Menu.Item
+                    key='home'
+                >
+                    <Link
+                        to='/'
+                    >
+                        <HomeOutlined />
+                        Home
+                    </Link>
+                </Menu.Item>
+                <Menu.Item
+                    key='profile'
+                >
+                    <Link
+                        to='/profile'
+                    >
+                        <ProfileOutlined />
+                        Profile
+                    </Link>
+                </Menu.Item>
+                <Menu.Item
+                    key='protected'
+                >
+                    <Link
+                        to='/protected'
+                    >
+                        <FileProtectOutlined />
+                        Protected
+                    </Link>
+                </Menu.Item>
+            </Menu>
+        </div>
+    );
+}
+
+export default Nav
